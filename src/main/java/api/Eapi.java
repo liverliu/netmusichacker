cda664be529fc9c20a15aef6a665857b47421b64
liverliu <shi753951@gmail.com>
2016-02-01 23:50:03 +0800

    Add Eapi
    Add Eapi

---
+++ src/main/java/api/Eapi.java
@@ -0,0 +1,12 @@
+package api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by liverliu on 2016/2/1.
+ */
+@Controller
+@RequestMapping("/api")
+public class Eapi extends BaseApi{
+}
